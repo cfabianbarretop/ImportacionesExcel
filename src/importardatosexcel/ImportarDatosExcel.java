f04f906f0c3c72cfbae7c05b2f439f4d51927ef5
Fabian <Fabian@DESKTOP-LMPDC2E>
2019-02-25 16:40:11 -0500

    primera

--- src/importardatosexcel/ImportarDatosExcel.java
+++ src/importardatosexcel/ImportarDatosExcel.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 public class ImportarDatosExcel {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        Cuentas. importarDatos("C:\\Users\\Fabian\\Documents\\Sigma\\UnionProgreso\\cuentasP.xlsx", 3, 8);
+        Cuentas. importarDatos("C:\\Users\\Fabian\\Documents\\Sigma\\UnionProgreso\\cuentasP.xlsx", 3, 8);
 //        CreditoEmergente.importarDatos("C:\\Users\\Fabian\\Documents\\Sigma\\UnionProgreso\\Cuotas_Créditos_Emergentes.xlsx", 3, 12, 4);
-        EstadoCuenta.importarDatos("C:\\Users\\Fabian\\Documents\\Sigma\\JardinAzuayo\\Estado_Cuenta.xlsx", 1, 14, 4);
+//        EstadoCuenta.importarDatos("C:\\Users\\Fabian\\Documents\\Sigma\\JardinAzuayo\\Estado_Cuenta.xlsx", 1, 14, 4);
     }
 }
